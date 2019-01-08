7c1c320b029bd0b91fc4b951a0459617be140951
ranajeet2205 <ranajeet.barik96@gmail.com>
2019-01-08 21:21:05 +0530

    Initial commit

---
+++ app/src/main/java/com/stlindia/navdrawer/fragments/FragmentSlideShow.java
@@ -0,0 +1,20 @@
+package com.stlindia.navdrawer.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.stlindia.navdrawer.R;
+
+public class FragmentSlideShow extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_slideshow,container,false);
+    }
+}
